f90ece8567c88e8d5a007989957fc3b3eb8a599c
357016138 <357016138@qq.com>
2017-10-26 18:42:31 +0800

    产看activity生命周期（正常和异常）

--- app/src/main/java/com/song/androidart1/MainActivity.java
+++ app/src/main/java/com/song/androidart1/MainActivity.java
@@ -1,13 +1,108 @@
 package com.song.androidart1;
 
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+/**
+ * 一个查看activity生命周期的例子，可以查看正常情况和异常情况(屏幕旋转或被杀死等)的周期
+ *
+ * */
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            String test = savedInstanceState.getString("extra_test");
+            Log.d(TAG, "[onCreate]restore extra_test:" + test);
+        }
+
+//        TextView
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent("com.ryg.charpter_1.c");
+                Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+//                intent.setClass(MainActivity.this, SecondActivity.class);
+//                intent.putExtra("time", System.currentTimeMillis());
+//                intent.addCategory("com.ryg.category.c");
+//                intent.setDataAndType(Uri.parse("file://abc"), "text/plain");
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d(TAG, "onConfigurationChanged, newOrientation:" + newConfig.orientation);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        outState.putString("extra_test", "test");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        //Log.d(TAG, "onRestoreInstanceState");
+        String test = savedInstanceState.getString("extra_test");
+        Log.d(TAG, "[onRestoreInstanceState]restore extra_test:" + test);
     }
 }
